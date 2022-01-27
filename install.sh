f73d5cfda5eaeca84c94b227ad3f622eef76ce32
Ali Ghahremani <alighahremani1377@gmail.com>
2022-01-27 11:00:20 +0330

    relaese link update to 1.1.0

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 
 # download
 echo "downloading..."
-wget -q -O - https://github.com/ali77gh/shecan-cli/releases/download/1.0.1/shecan.py > temp.py
+wget -q -O - https://github.com/ali77gh/shecan-cli/releases/download/1.1.0/shecan.py > temp.py
 echo "done"
 
 # copy
